08b66dc05b18511dcb39edbe25c83e157c1a2608
cyz12 <1093491992@qq.com>
2020-04-22 18:31:34 +0800

    project3

---
+++ project_three/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 projectthree.rc 使用
+//
+#define IDC_EIDT_QUERY_BY_NAME          3
+#define IDC_EIDT_QUERY_BY_IP            4
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PROJECT_THREE_DIALOG        102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_NAME                   1000
+#define IDC_EDIT_IP                     1001
+#define IDC_EDIT_IP2                    1002
+#define IDC_EDIT_HOST_INFORMATION       1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
